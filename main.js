4be888cadd2e9f1153b761b88f539ff7cbadd61d
billyGitHup <billy.meas@yahoo.com>
2024-11-25 13:14:52 -0600

    Instructor comments

--- main.js
+++ main.js
@@ -3,3 +3,6 @@
 console.log('This is my first sentence');
 console.log('I am new to JavaScript');
 console.log('I am learning stuff every day');
+
+
+// Instructor's comments: Very good, Ethan! Keep up with the good work and keep practicing.

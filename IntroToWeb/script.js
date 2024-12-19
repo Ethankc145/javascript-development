e18e66df4cc32d4a151e769e141f17ac2e61ad83
Ethankc145 <ethankchuynh@gmail.com>
2024-12-19 16:32:23 -0600

    Intro To Web Assignment

---
+++ IntroToWeb/script.js
@@ -0,0 +1,20 @@
+// const elementTexts = document.getElementById('first').innerText;
+document.getElementById('first').innerText = 
+'This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph. This is a new paragraph.';
+
+const pElement = document.getElementById('first')
+pElement.style.color='red';
+pElement.style.fontSize = "25px"
+
+const elements = document.getElementsByClassName('cool-class')
+console.log(elements);
+
+const elementsTwo = document.querySelectorAll('.cool-class')
+console.log(elementsTwo);
+
+elementsTwo.forEach((element) => {
+    element.style.color = "orange";
+});
+
+const pEl = document.querySelectorAll('p')
+console.log(pEl);
